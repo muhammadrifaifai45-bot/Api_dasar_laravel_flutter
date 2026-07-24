c8b1753c7bb9db155571248d4ac26e52bd78d01d
rasyanaj09-glitch <rasyanaj09@gmail.com>
2026-07-24 13:39:44 +0700

    revision file

--- lib/layar/home_page.dart
+++ lib/layar/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:tokoxiezz/models/product.dart';
+import 'package:tokoxiezz/services/setingan_api.dart';
+import '../models/product.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,8 +11,29 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final SetingganApi api=SetingganApi();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Data Produk Xiezz Shopp"),
+        centerTitle: true,
+      ),
+      body: FutureBuilder<List<Product>>(
+        future: api.getProducts(), 
+        builder: (context, snapshot){
+          if(snapshot.connectionState==ConnectionState.waiting){
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if(snapshot.hasError){
+            return Center(
+              child: Text(snapshot.error.toString()),
+            );
+          }
+        }
+        ),
+    );
   }
 }
