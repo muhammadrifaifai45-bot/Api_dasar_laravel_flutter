78604331c0f59f12d72583545b083abf75bbb135
rasyanaj09-glitch <rasyanaj09@gmail.com>
2026-07-24 10:22:49 +0700

    first api

---
+++ lib/models/product.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class Product{
+  int? id;
+  String nama;
+  double harga;
+  int stock;
+  String deskripsi;
+  String? gambar;
+  Product({
+    this.id,
+    required this.nama,
+    required this.harga,
+    required this.stock,
+    required this.deskripsi,
+    this.gambar
+  });
+  factory Product.fromJson(Map<String,dynamic>json){
+    return Product(
+      id:json['id'],
+      nama:json['nama'],
+      harga:json['harga'],
+      stock:json['stock'],
+      deskripsi:json['deskripsi'],
+      gambar:json['gambar']
+    );
+  }
+  
+  Map<String,dynamic>toJson(){
+    return{
+      "id":id,
+      "nama":nama,
+      "harga":harga,
+      "stock":stock,
+      "deskripsi":deskripsi,
+      "gambar":gambar,
+    };
+  }
+  
+}
