3cfac1a1903216f9d480b63115bf134a697c13b6
rasyanaj09-glitch <rasyanaj09@gmail.com>
2026-08-28 11:26:25 +0700

    debuging dan menambahkan function untuk menampilkan sebuah data dan mencari data serta memangil function pencarian

--- lib/layar/home_page.dart
+++ lib/layar/home_page.dart
@@ -5,6 +5,7 @@ import 'package:tokoxiezz/layar/detail_product.dart';
 import 'package:tokoxiezz/layar/edit_page.dart';
 import 'package:tokoxiezz/models/product.dart';
 import 'package:tokoxiezz/services/setingan_api.dart';
+import 'package:tokoxiezz/widget/dashboard_card.dart';
 import 'package:tokoxiezz/widget/product_card.dart';
 
 class HomePage extends StatefulWidget {
@@ -20,13 +21,14 @@ class _HomePageState extends State<HomePage> {
   List<Product> semuaproduk = [];
   List<Product> hasilpencarian = [];
 
-  Future<List<Product>>?futureProduk;
+  Future<List<Product>>? futureProduk;
 
-  void initState(){
+  void initState() {
     super.initState();
 
-    futureProduk=api.getProducts();
+    futureProduk = api.getProducts();
   }
+
   void Cariproduk(String keyword) {
     hasilpencarian = semuaproduk.where((produk) {
       return produk.nama.toLowerCase().contains(keyword.toLowerCase());
@@ -34,20 +36,23 @@ class _HomePageState extends State<HomePage> {
     setState(() {});
   }
 
-  double totalstok(){
-    return  semuaproduk.fold(
-      0,(total,item)=>total+(item.harga * item.stock),
-    );
-  }
-  double totalnilai(){
-    return  semuaproduk.fold(
-      0,(total,item)=>total+(item.harga * item.stock),
+  double totalstok() {
+    return semuaproduk.fold(
+      0,
+      (total, item) => total + (item.harga * item.stock),
     );
   }
 
-  void refreshData(){
+  double totalnilai() {
+    return semuaproduk.fold(
+      0,
+      (total, item) => total + (item.harga * item.stock),
+    );
+  }
+
+  void refreshData() {
     setState(() {
-      futureProduk=api.getProducts();
+      futureProduk = api.getProducts();
     });
   }
 
@@ -86,10 +91,10 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
- void dispose(){
-  searchController.dispose();
-  super.dispose();
- } 
+  void dispose() {
+    searchController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -130,28 +135,28 @@ class _HomePageState extends State<HomePage> {
                 return ListView(
                   physics: const AlwaysScrollableScrollPhysics(),
                   children: [
-                    SizedBox(height:MediaQuery.of(context)
-                    .size
-                    .height*
-                    0.25,
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.25,
                     ),
                     Center(
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          const Icon(Icons.error,
-                          size: 80,
-                          color: Colors.red,
+                          const Icon(
+                            Icons.error,
+                            size: 80,
+                            color: Colors.red,
                           ),
                           const SizedBox(height: 15),
-                          Padding(padding: const EdgeInsets.symmetric(horizontal: 20),
-                          child: Text(snapshot.error.toString(),
-                          textAlign: TextAlign.center
-                          ),  
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 20),
+                            child: Text(snapshot.error.toString(),
+                                textAlign: TextAlign.center),
                           ),
                           const SizedBox(height: 15),
-                          ElevatedButton(onPressed: refreshData,
-                          child: const Text("Gagal Harap Coba Lagi"),
+                          ElevatedButton(
+                            onPressed: refreshData,
+                            child: const Text("Gagal Harap Coba Lagi"),
                           ),
                         ],
                       ),
@@ -159,40 +164,151 @@ class _HomePageState extends State<HomePage> {
                   ],
                 );
               }
-              return ListView.builder(
-                itemCount: snapshot.data!.length,
-                itemBuilder: (context, index) {
-                  return ProductCard(
-                    product: snapshot.data![index],
-                    onEdit: () async {
-                      final hasil = await Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (_) => EditPage(
-                            product: snapshot.data![index],
+
+              if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                semuaproduk = [];
+                hasilpencarian = [];
+                return ListView(
+                  physics: const AlwaysScrollableScrollPhysics(),
+                  children: [
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.25,
+                    ),
+                    Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Icon(
+                            Icons.inventory,
+                            size: 80,
+                            color: Colors.green,
                           ),
-                        ),
-                      );
-                      if (hasil == true) {
-                        setState(() {});
-                      }
-                    },
-                    onDelete: () async {
-                      await hapusProduct(snapshot.data![index]);
-                    },
-                    onDetail: () async {
-                      await Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (_) =>
-                              DetailProduct(product: snapshot.data![index]),
-                        ),
-                      );
-                      setState(() {});
-                    },
-                  );
-                },
+                          const Text(
+                            "Berhasil Mnegambil data",
+                            style: TextStyle(
+                              fontSize: 20,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                );
+              }
+
+              semuaproduk = snapshot.data!;
+              if (searchController.text.isEmpty) {
+                hasilpencarian = semuaproduk;
+              }
+
+              return ListView(
+                physics: const AlwaysScrollableScrollPhysics(),
+                padding: const EdgeInsets.all(12),
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "HALLO BUDY",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 5),
+                      const Text(
+                        "SELAMAT DATANG PADA APLIKASI TOKO XIEZZ",
+                        style: TextStyle(fontSize: 14, color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 15),
+                  Row(
+                    children: [
+                      Expanded(
+                          child: DashboardCard(
+                              title: "produk",
+                              value: "${semuaproduk.length}",
+                              icon: Icons.shopping_bag,
+                              color: Colors.blueAccent)),
+                      Expanded(
+                          child: DashboardCard(
+                              title: "stok",
+                              value: "${totalstok()}",
+                              icon: Icons.inventory,
+                              color: Colors.blueAccent)),
+                      Expanded(
+                          child: DashboardCard(
+                              title: "nilai",
+                              value: "${totalnilai()}",
+                              icon: Icons.payment,
+                              color: Colors.green)
+                              ),
+                    ],
+                  ),
+                  const SizedBox(height: 15),
+                  TextField(controller: searchController,
+                  onChanged: Cariproduk,
+                  decoration: InputDecoration(
+                    hintText: "Cari produk yang anda inginkan ",
+                    prefixIcon: const Icon(Icons.search,
+                    ),
+                    suffixIcon: searchController
+                    .text
+                    .isNotEmpty
+                    ?IconButton(
+                      icon: const Icon(Icons.clear),
+                      onPressed: (){
+                        searchController.clear();
+                        Cariproduk("");
+                      },
+                      )
+
+                      :null,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+
+                  ),
+                  ),
+                  const SizedBox(height: 15,)
+                ],
               );
+
+            
+
+              // return ListView.builder(
+              //   itemCount: snapshot.data!.length,
+              //   itemBuilder: (context, index) {
+              //     return ProductCard(
+              //       product: snapshot.data![index],
+              //       onEdit: () async {
+              //         final hasil = await Navigator.push(
+              //           context,
+              //           MaterialPageRoute(
+              //             builder: (_) => EditPage(
+              //               product: snapshot.data![index],
+              //             ),
+              //           ),
+              //         );
+              //         if (hasil == true) {
+              //           setState(() {});
+              //         }
+              //       },
+              //       onDelete: () async {
+              //         await hapusProduct(snapshot.data![index]);
+              //       },
+              //       onDetail: () async {
+              //         await Navigator.push(
+              //           context,
+              //           MaterialPageRoute(
+              //             builder: (_) =>
+              //                 DetailProduct(product: snapshot.data![index]),
+              //           ),
+              //         );
+              //         setState(() {});
+              //       },
+              //     );
+              //   },
+              // );
             }),
       ),
       floatingActionButton: FloatingActionButton(
